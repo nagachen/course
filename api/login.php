0563b6c05271b0b67765408ba63e6001f924b623
nagachen <birdann99@gmail.com>
2023-06-01 21:15:32 +0800

    初步的login 完成

--- api/login.php
+++ api/login.php
@@ -2,14 +2,21 @@
 <?php
 include "../db.php";
 dd($_POST);
-$rows=$pdo->query("selete * from `student`")->fetchAll(PDO::FETCH_ASSOC);
-if(isset($_POST)){
-    foreach($rows as $row){
-        if($_POST['acc']){
-
-        }
-    }
-    
+$chk=$pdo->query("select count(*) from `student` 
+                    where `acc` = '{$_POST['acc']}' && `pw` = '{$_POST['pw']}'")
+->fetchColumn();
+dd($chk);
+if($chk){         //驗證是否成功？
+   //身份和登入成功寫入
+   $power=$pdo->query("select `power` from `student`
+                       where `acc`='{$_POST['acc']}' 
+                       && `pw` = '{$_POST['pw']}'")->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['power']=$power;
+    $_SESSION['login']=$_POST['acc'];
+    // echo"登入成功";
+       header("location:?do=sucess");
+}else{
+       header("location:?do=error");
 }
 
 
--- backend.php
+++ backend.php
@@ -1,15 +1,6 @@
 <!-- 後台頁面 -->
-<?php include_once "db.php";
-$do='';
-if(isset($_GET['do'])){
-    $do=$_GET['do'];
-}else{
-    
-        $do="error";
-  
-}
-
-
+<?php
+include_once "db.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,6 +18,20 @@ if(isset($_GET['do'])){
 </head>
 <body>
     <?php
+    
+    //需登入才可以用選單
+    //沒有登入只能看一些無關緊要的資訊呈現
+    // 登入後同時將進行身份驗證，使用session,並展現可以操作的相關資訊(一連串的驗證)
+    $do='';
+    if(isset($_GET['do'])){
+        $do=$_GET['do'];
+    }else{
+        
+            $do="error";
+      
+    }
+    
+    
     
     include_once "header.php";
     include "./front/login.php";
